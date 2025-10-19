916e5ede60016db90b3fc5ae82273aa697e61dbc
kamil-perczynski <kamil.perczynski.95@gmail.com>
2025-10-19 10:42:46 +0200

    feat: automatic kube node cleanup

--- src/test/kotlin/pl/kperczynski/kube_spot_operator/domain/NodeCleanupsTest.kt
+++ src/test/kotlin/pl/kperczynski/kube_spot_operator/domain/NodeCleanupsTest.kt
@@ -11,13 +11,13 @@ class NodeCleanupsTest {
     val nodes = listOf(
       KubeNode(
         name = "node-1",
-        conditions = listOf("Ready"),
-        taints = emptyList()
+        conditions = listOf(),
+        taints = listOf("node.kubernetes.io/unschedulable")
       ),
       KubeNode(
         name = "node-2",
-        conditions = emptyList(),
-        taints = listOf("node.kubernetes.io/unschedulable")
+        conditions = listOf("Ready"),
+        taints = listOf()
       ),
       KubeNode(
         name = "node-3",
@@ -37,8 +37,8 @@ class NodeCleanupsTest {
     // then
     assertThat(result).containsExactly(
       ScheduledNodeDelete(
-        nodeName = "node-2",
-        executionerNode = "node-3"
+        nodeName = "node-1",
+        executionerNode = "node-2"
       )
     )
   }
@@ -163,4 +163,46 @@ class NodeCleanupsTest {
     // then
     assertThat(result).isEmpty()
   }
+
+  @Test
+  fun `should find executioner node moving backwards`() {
+    val nodes = listOf(
+      KubeNode(
+        name = "ip-10-46-101-99.eu-north-1.compute.internal",
+        conditions = listOf("Ready"),
+        taints = emptyList()
+      ),
+      KubeNode(
+        name = "ip-10-46-102-40.eu-north-1.compute.internal",
+        conditions = listOf("Ready"),
+        taints = emptyList()
+      ),
+      KubeNode(
+        name = "ip-10-46-103-173.eu-north-1.compute.internal",
+        conditions = listOf("Ready"),
+        taints = emptyList()
+      ),
+      KubeNode(
+        name = "ip-10-46-103-44.eu-north-1.compute.internal",
+        conditions = emptyList(),
+        taints = listOf(
+          "node.kubernetes.io/unschedulable",
+          "node.kubernetes.io/unreachable",
+          "node.kubernetes.io/unreachable"
+        )
+      )
+    )
+
+    // when
+    val result = findNodesToDelete(nodes)
+
+    // then
+    assertThat(result).containsExactly(
+      ScheduledNodeDelete(
+        nodeName = "ip-10-46-103-44.eu-north-1.compute.internal",
+        executionerNode = "ip-10-46-103-173.eu-north-1.compute.internal"
+      )
+    )
+  }
+
 }
