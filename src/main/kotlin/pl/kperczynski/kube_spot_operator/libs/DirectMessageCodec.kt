11deba32ffaeb2bbb7623ad0a0b1d4485cd1d9fd
kamil-perczynski <kamil.perczynski.95@gmail.com>
2026-06-14 00:49:18 +0200

    chore: upgrade to JDK 25 and fix compiler warnings

--- src/main/kotlin/pl/kperczynski/kube_spot_operator/libs/DirectMessageCodec.kt
+++ src/main/kotlin/pl/kperczynski/kube_spot_operator/libs/DirectMessageCodec.kt
@@ -3,22 +3,22 @@ package pl.kperczynski.kube_spot_operator.libs
 import io.vertx.core.buffer.Buffer
 import io.vertx.core.eventbus.MessageCodec
 
-class DirectMessageCodec : MessageCodec<Object, Object> {
+class DirectMessageCodec : MessageCodec<Any, Any> {
 
   companion object {
     const val DIRECT_CODEC_NAME = "Direct"
   }
 
-  override fun encodeToWire(buffer: Buffer?, s: Object?) {
+  override fun encodeToWire(buffer: Buffer?, s: Any?) {
     throw UnsupportedOperationException("Not implemented")
   }
 
-  override fun decodeFromWire(pos: Int, buffer: Buffer?): Object {
+  override fun decodeFromWire(pos: Int, buffer: Buffer?): Any {
     throw UnsupportedOperationException("Not implemented")
   }
 
-  override fun transform(s: Object?): Object {
-    return s as Object
+  override fun transform(s: Any?): Any {
+    return s as Any
   }
 
   override fun name(): String {
